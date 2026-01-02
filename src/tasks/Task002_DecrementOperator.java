8585a89d8d99c3b1ae2a4256e7713853a9f79152
Naval <navalchaudhary989@gmail.com>
2026-01-02 08:50:27 +0530

    Updated

--- src/tasks/Task002_DecrementOperator.java
+++ src/tasks/Task002_DecrementOperator.java
@@ -4,8 +4,10 @@ public class Task002_DecrementOperator {
     public static void main(String[] args) {
         int a= 10;
         int b= a-- - --a + a--;
+        int c= a++ + a--;
 
-        System.out.printf("Value of a= %d",a);
+        System.out.printf("\nValue of a= %d",a);
         System.out.printf("\nValue of b= %d",b);
+        System.out.println("\nValue of c= "+c);
     }
 }
