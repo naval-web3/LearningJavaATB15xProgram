7771c70d6aede1e5f4dca951d09048c74cc3d5c2
Naval <navalchaudhary989@gmail.com>
2026-01-06 10:23:58 +0530

    Updated

--- src/tasks/Task005_Ifelse.java
+++ src/tasks/Task005_Ifelse.java
@@ -2,6 +2,12 @@ package tasks;
 
 import java.util.Scanner;
 
+/* QWrite a program that classifies a triangle based on its side lengths.
+Given three input values representing the lengths of the sides, determine if the triangle is equilateral (all sides are equal),
+isosceles (exactly two sides are equal), or scalene (no sides are equal).
+Use an if-else statement to classify the triangle.*/
+
+
 public class Task005_Ifelse{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
