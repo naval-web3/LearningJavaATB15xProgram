23203a43312e44e4bcbcb81a77077c2555e0f2ed
Naval <navalchaudhary989@gmail.com>
2026-01-07 09:35:53 +0530

    Updated

--- src/ex_01_Java_Basics/Lab003_SwitchStmt.java
+++ src/ex_01_Java_Basics/Lab003_SwitchStmt.java
@@ -24,41 +24,45 @@ public class Lab003_SwitchStmt {
         Scanner input= new Scanner(System.in);
 
         System.out.println("Enter number between 1 to 7");
-        int days= input.nextInt();
 
-        switch(days){
+        if(input.hasNextInt()){
+            int days= input.nextInt();
+            switch(days) {
 
-            case 1:
-                System.out.println("Monday");
-                break;
+                case 1:
+                    System.out.println("Monday");
+                    break;
 
-            case 2:
-                System.out.println("Tuesday");
-                break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
 
-            case 3:
-                System.out.println("Wednesday");
-                break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
 
-            case 4:
-                System.out.println("Thursday");
-                break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
 
-            case 5:
-                System.out.println("Friday");
-                break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
 
-            case 6:
-                System.out.println("Saturday");
-                break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
 
-            case 7:
-                System.out.println("Sunday");
-                break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
 
-            default:
-                System.out.println("Invalid Input");
+                default:
+                    System.out.println("Invalid Input");
+            }
         }
-
+        else{
+            System.out.println("Invalid Input");
+        }
     }
 }
